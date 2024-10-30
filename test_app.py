808d94e48635ecc3048b8cd3b7c086e71775ce3c
Jamison-Lii <ntuvlii5@gmail.com>
2024-10-30 20:34:20 +0100

    yh

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from app import app
+
+
+class TestApp(unittest.TestCase):
+    def test_my_function(self):
+        result = app()
+# self.assertTrue(message, message)
+
+if _name == '_main_':
+    unittest.main()
+
