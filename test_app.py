6c9be4602ab48f4a476fcd7403fb828143db3703
Jamison-Lii <ntuvlii5@gmail.com>
2024-10-31 17:24:12 +0100

    we got this

--- test_app.py
+++ test_app.py
@@ -1,12 +1,25 @@
+# test_app.py
 import unittest
-from app import app
+from flask import Flask , jsonify
+#from test_app import app
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
 class TestApp(unittest.TestCase):
-    def test_my_function(self):
-        result = app()
-# self.assertTrue(message, message)
+    def setUp(self):
+        self.app = app.test_client()
 
-if _name == '_main_':
+    def test_home_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"message": "Hello level 400 FET, Quality Assurance!"})
+
+
+if __name__ == '__main__':
     unittest.main()
-
