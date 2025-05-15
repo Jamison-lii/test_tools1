ddd601d25862a8aebee8268e172eb7ab171ded9d
Jamison-lii <ntuvlii5@gmail.com>
2025-05-15 07:27:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('my_project_env')
 def home():
-    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+    return jsonify(message="Hello level 400 FET, Quality Assurance")
 
 
 if __name__ == '__main__':
